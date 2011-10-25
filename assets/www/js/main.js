be27a2048be67228bda70f522d47ea625d6994cd
Raymond Camden <raymondcamden@gmail.com>
2011-10-25 16:09:10 -0500

    misc

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,11 +1,41 @@
 var contactOb;
+var positionOb;
+var beaconURL;
 
 function init() {
     document.addEventListener("deviceready", phoneReady, false);
 }
 
 function phoneReady() {
+    //first step is to get GPS
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, {maximumAge:0, enableHighAccurage:true});
 
+    //generic error handler.
+    function displayError(str) {
+        x$("#errorDiv").html(str);
+    }
+    
+    function geoSuccess(position) {
+        //copy it over
+        positionOb = position;
+
+        //before we tell the user anything - we need to register our location for a beacon id
+        x$("#currentStatus").xhr("http://192.168.1.105/beacon/service/remote.cfc?method=registerBeacon&returnFormat=plain&longitude="+position.coords.longitude+"&latitude="+position.coords.latitude, {
+                async:false,
+                method:"get",
+                callback:function() {
+                    beaconURL = this.responseText;
+                }
+            });
+
+        x$("#currentStatus").html("after","Sweet! Got your location, now I need you to pick a contact.");
+        x$("#contactButtonBlock").attr("style","");
+    }
+    
+    function geoError() {
+        displayError("Sorry - an error with GPS was thrown and I can't do anything else.");
+    }
+    
     x$("#pickContactButton").touchstart(function() {
         window.plugins.contactView.show(
             function(contact) {
@@ -15,6 +45,7 @@ function phoneReady() {
                     
                     //copy to global var
                     contactOb = contact;
+                    
                     var s = "<p>You will be sending "+contact.name +" information on how to reach you.</p>";
                     if(typeof contact.phone === "undefined") {
                         s+= "<button disabled='true'>Send SMS</button>";
@@ -29,11 +60,11 @@ function phoneReady() {
                     x$("#contactChosen").html(s);
 
                     x$("#sendSMSButton").touchstart(function() {
-                       var body = "Find me by cliking here: ";
+                       var body = "Find me by cliking here: "+beaconURL;
                        var theUrl = "sms:"+contactOb.phone+"?body="+escape(body);
                        window.location.href=theUrl;
                    });
-                    
+                   
 
                 }
             },
